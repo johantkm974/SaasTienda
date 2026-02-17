1ff9e4d44019a9db525f9bf697034aea99e73a1d
Johan Vasquez Ricapa <johanvasqez20@gmail.com>
2026-02-16 22:01:07 -0500

    Update EmpresaService.java

--- backend/src/main/java/com/example/demo/service/EmpresaService.java
+++ backend/src/main/java/com/example/demo/service/EmpresaService.java
@@ -14,12 +14,31 @@ public class EmpresaService {
     private final EmpresaRepository empresaRepository;
 
     public Empresa registrar(Empresa empresa) {
+        empresa.setId(null);
         empresaRepository.findByRuc(empresa.getRuc())
                 .ifPresent(e -> {
                     throw new RuntimeException("Ya existe una empresa con ese RUC");
                 });
 
         return empresaRepository.save(empresa);
+    }
+    public Empresa actualizar(Integer id, Empresa empresaActualizada){
+        return empresaRepository.findById(id).map(empresaExistente ->{
+
+            if (!empresaExistente.getRuc().equals(empresaActualizada.getRuc())){
+                empresaRepository.findByRuc(empresaActualizada.getRuc())
+                    .ifPresent(e-> {
+                        throw new RuntimeException("Ya existe otra empresa usando es nuevo Ruc");
+                    });
+            }
+            empresaExistente.setNombreComercial(empresaActualizada.getNombreComercial());
+            empresaExistente.setRuc(empresaActualizada.getRuc());
+            empresaExistente.setEstado(empresaActualizada.getEstado());
+            empresaExistente.setLogoUrl(empresaActualizada.getLogoUrl());
+            empresaExistente.setTelefono(empresaActualizada.getTelefono());
+
+            return empresaRepository.save(empresaExistente);
+        }).orElseThrow(()->new RuntimeException("Empresa no encontrada con el ID" +id));
     }
 
     public List<Empresa> listar() {
@@ -30,4 +49,11 @@ public class EmpresaService {
         return empresaRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Empresa no encontrada"));
     }
+    public void eliminar(Integer id){
+        if(!empresaRepository.existsById(id)){
+            throw new RuntimeException("No se puede eliminar la empresa no existe");
+        }
+        empresaRepository.deleteById(id);
+    }
 }
+
