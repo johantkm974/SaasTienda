8789f1577f9c871e43ebd8c531da9c5a0e2cbdea
Johan Vasquez Ricapa <johanvasqez20@gmail.com>
2026-02-16 22:00:07 -0500

    Update ProveedorService.java

--- backend/src/main/java/com/example/demo/service/ProveedorService.java
+++ backend/src/main/java/com/example/demo/service/ProveedorService.java
@@ -13,12 +13,40 @@ public class ProveedorService {
 
     private final ProveedorRepository proveedorRepository;
 
-    public Proveedor guardar(Proveedor proveedor) {
+    public Proveedor crear(Proveedor proveedor){
+        proveedor.setId(null);
         return proveedorRepository.save(proveedor);
+    }
+    public Proveedor actualizar(Integer id, Proveedor proveedorActualizado){
+        return proveedorRepository.findById(id).map(proveedorExistente->{
+
+            proveedorExistente.setNombre(proveedorActualizado.getNombre());
+            proveedorExistente.setIdentificacion(proveedorActualizado.getIdentificacion());
+            proveedorExistente.setTelefono(proveedorActualizado.getTelefono());
+            proveedorExistente.setCorreo(proveedorActualizado.getCorreo());
+
+            proveedorExistente.setEmpresa(proveedorActualizado.getEmpresa());
+
+            return proveedorRepository.save(proveedorExistente);
+
+        }).orElseThrow(()-> new RuntimeException("Proveedor no enocntrado con el ID"+ id));
     }
 
     public List<Proveedor> listarPorEmpresa(Integer empresaId) {
         return proveedorRepository.findByEmpresaId(empresaId);
+    }
+    public List<Proveedor> listarTodos(){
+        return proveedorRepository.findAll();
+    }
+    public Proveedor buscarPorId(Integer id){
+        return proveedorRepository.findById(id).orElseThrow(()->new RuntimeException("Proveedor no encontrado con el id"+ id)); 
+    }
+
+    public void eliminar(Integer id){
+        if(!proveedorRepository.existsById(id)){
+            throw new RuntimeException("No se puede eliminar, el proveedor no existe");
+        }
+        proveedorRepository.deleteById(id);
     }
 }
 
