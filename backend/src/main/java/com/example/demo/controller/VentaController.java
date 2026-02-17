02ad38dd74821593ab07692e47ade4707d4a8932
Johan Vasquez Ricapa <johanvasqez20@gmail.com>
2026-02-16 21:57:08 -0500

    Update VentaController.java

--- backend/src/main/java/com/example/demo/controller/VentaController.java
+++ backend/src/main/java/com/example/demo/controller/VentaController.java
@@ -5,6 +5,8 @@ import com.example.demo.service.VentaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/ventas")
 @RequiredArgsConstructor
@@ -12,8 +14,27 @@ public class VentaController {
 
     private final VentaService ventaService;
 
+    // CREAR VENTA (El que ya tenías)
     @PostMapping
     public Venta registrar(@RequestBody Venta venta) {
         return ventaService.registrarVenta(venta);
+    }
+
+    // VER EL RECIBO DE UNA VENTA ESPECÍFICA
+    @GetMapping("/{id}")
+    public Venta buscar(@PathVariable Integer id) {
+        return ventaService.buscarPorId(id);
+    }
+
+    // VER TODAS LAS VENTAS DE UNA EMPRESA
+    @GetMapping("/empresa/{empresaId}")
+    public List<Venta> listarPorEmpresa(@PathVariable Integer empresaId) {
+        return ventaService.listarPorEmpresa(empresaId);
+    }
+
+    // VER TODAS LAS COMPRAS DE UN CLIENTE
+    @GetMapping("/usuario/{usuarioId}")
+    public List<Venta> listarPorUsuario(@PathVariable Integer usuarioId) {
+        return ventaService.listarPorUsuario(usuarioId);
     }
 }
