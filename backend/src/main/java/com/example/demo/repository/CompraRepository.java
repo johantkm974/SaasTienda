708dd2cef3fd3c15b2e2d3c246ed3778d45da937
Johan Vasquez Ricapa <johanvasqez20@gmail.com>
2026-02-17 21:20:03 -0500

    Delete backend/src/main/java/com/example/demo/repository directory

--- backend/src/main/java/com/example/demo/repository/CompraRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-import com.example.demo.model.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import java.util.*;
-@Repository
-public interface CompraRepository extends JpaRepository<Compra, Integer> {
-    List<Compra> findByEmpresaId(Integer empresaId);
-}
