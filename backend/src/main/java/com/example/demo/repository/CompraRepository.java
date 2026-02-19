c53ecfb0cef1dd829bf588d03f2e6a8f5bdd8402
Johan Vasquez Ricapa <johanvasqez20@gmail.com>
2026-02-18 23:26:35 -0500

    Update CompraRepository.java

--- backend/src/main/java/com/example/demo/repository/CompraRepository.java
+++ backend/src/main/java/com/example/demo/repository/CompraRepository.java
@@ -1,9 +1,16 @@
 package com.example.demo.repository;
+
 import com.example.demo.model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.*;
+
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CompraRepository extends JpaRepository<Compra, Integer> {
+
     List<Compra> findByEmpresaId(Integer empresaId);
+
+    Optional<Compra> findByIdAndEmpresaId(Integer id, Integer empresaId);
 }
