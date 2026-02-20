6a5df2173a869910f5d31944bc28b608238712d9
johantkm974 <johanvasqez20@gmail.com>
2026-02-19 23:59:55 -0500

    borre el src

--- backend/src/main/java/com/example/demo/repository/RolRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-import com.example.demo.model.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RolRepository extends JpaRepository<Rol, Integer> {
-    // Los roles suelen ser globales, así que el findAll() estándar sirve
-}
